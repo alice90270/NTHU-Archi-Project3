f70b15de427f22a893bc718593c44fb3ac6b33e1
Alice Tsai <alice90270@gmail.com>
2016-05-15 02:31:14 +0800

    no message

---
+++ module/ForwardinUnit.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+/*******************************************************************
+ * Create Date: 	2016/05/03
+ * Design Name: 	Pipeline CPU
+ * Module Name:		ForwardinUnit 
+ * Project Name: 	Architecture Project_3 Pipeline CPU
+ 
+ * Please DO NOT change the module name, or your'll get ZERO point.
+ * You should add your code here to complete the project 3 (bonus.)
+ ******************************************************************/
+module ForwardinUnit(EX_MEMRegWrite,MEM_WBRegWrite,EX_MEMRegisterRd,MEM_WBRegisterRd,ID_EXRegisterRs,ID_EXRegisterRt,ForwardA,ForwardB);
+input EX_MEMRegWrite,MEM_WBRegWrite;
+input [4:0] EX_MEMRegisterRd,MEM_WBRegisterRd,ID_EXRegisterRs,ID_EXRegisterRt;
+output reg [1:0] ForwardA,ForwardB;
+
+always@(*)begin
+
+// add your code here
+
+end
+
+
+endmodule
